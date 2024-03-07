5b173ff589040324582c7b521c7c8193d40a8495
dondycles <johnrod.dondoyano@gmail.com>
2024-03-07 13:24:33 +0800

    modified:   app/(user)/activity/page.tsx

--- app/(user)/activity/page.tsx
+++ app/(user)/activity/page.tsx
@@ -4,24 +4,16 @@ import { useQuery } from "@tanstack/react-query";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { usePhpPeso } from "@/lib/php-formatter";
 import { FaPesoSign } from "react-icons/fa6";
-import { ArrowBigRightDash, ArrowRight, Circle } from "lucide-react";
 type MoneyJSONData = {
   amount: number;
   name: string;
@@ -41,17 +33,28 @@ export default function Activity() {
   const logs = logsData?.success?.flatMap((log) => log);
 
   const action = (action: string | null) => {
-    return (
-      <Circle
-        className={` text-background dark:text-foreground  stroke-none size-3 m-auto
-            ${action === "add_money" && "fill-green-500 hover:fill-green-600"}
-            ${
-              action === "edit_money" && "fill-yellow-500 hover:fill-yellow-600"
-            }
-            ${action === "del_money" && "fill-red-500 hover:fill-red-600"}
-            `}
-      />
-    );
+    switch (action) {
+      case "add_money":
+        return (
+          <Badge className="bg-green-500 text-background dark:text-foreground hover:bg-green-600 w-full">
+            Add
+          </Badge>
+        );
+
+      case "edit_money":
+        return (
+          <Badge className="bg-yellow-500 text-background dark:text-foreground hover:bg-yellow-600 w-full">
+            Edit
+          </Badge>
+        );
+
+      case "del_money":
+        return (
+          <Badge className="bg-red-500 text-background dark:text-foreground hover:bg-red-600 w-full">
+            Del
+          </Badge>
+        );
+    }
   };
 
   return (
@@ -60,104 +63,90 @@ export default function Activity() {
         <CardHeader>
           <CardTitle className="text-2xl">Logs</CardTitle>
         </CardHeader>
-        <CardContent className="grid">
-          <ScrollArea>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead />
-                  <TableHead>Name</TableHead>
-                  <TableHead>Date</TableHead>
-                  <TableHead>Changes (if edit)</TableHead>
-                  <TableHead>Total Money</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {logs?.map((log) => {
-                  return (
-                    <TableRow key={log.id}>
-                      <TableCell>{action(log.action)}</TableCell>
-                      <TableCell>
-                        {(log.latest_data as MoneyJSONData)?.name}
-                      </TableCell>
-                      <TableCell>
-                        {new Date(log.created_at).toLocaleString()}
-                      </TableCell>
-                      <TableCell className="grid">
-                        {log.action === "edit_money" && log.last_data ? (
-                          <div className="grid grid-cols-3">
-                            {(log.last_data as MoneyJSONData).amount !==
-                            (log.latest_data as MoneyJSONData).amount ? (
-                              <>
-                                <div className="flex items-center min-w-fit">
-                                  <FaPesoSign className="text-xs min-w-fit" />
-                                  {usePhpPeso(
-                                    (log.last_data as MoneyJSONData).amount
-                                  )}{" "}
-                                </div>
-                                <div>
-                                  <ArrowBigRightDash
-                                    className={`m-auto ${
-                                      (log.last_data as MoneyJSONData).amount >=
-                                      (log.latest_data as MoneyJSONData).amount
-                                        ? "text-red-500"
-                                        : "text-green-500"
-                                    } `}
-                                  />
-                                </div>
-                                <div className="flex items-center">
-                                  <FaPesoSign className="text-xs min-w-fit" />{" "}
-                                  {usePhpPeso(
-                                    (log.latest_data as MoneyJSONData).amount
-                                  )}
-                                </div>
-                              </>
-                            ) : null}
-                            {(log.last_data as MoneyJSONData).name !==
-                            (log.latest_data as MoneyJSONData).name ? (
-                              <>
-                                <span>
-                                  {(log.last_data as MoneyJSONData).name}{" "}
-                                </span>
-                                <ArrowBigRightDash className="m-auto" />
-                                <span>
-                                  {(log.latest_data as MoneyJSONData).name}
-                                </span>
-                              </>
-                            ) : null}
+        <ScrollArea>
+          <CardContent className="grid max-h-[500px] overflow-auto">
+            <ScrollArea>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead />
+                    <TableHead>Name</TableHead>
+                    <TableHead>Date</TableHead>
+                    <TableHead>Changes</TableHead>
+                    <TableHead>Total Money</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {logs?.map((log) => {
+                    return (
+                      <TableRow key={log.id}>
+                        <TableCell>{action(log.action)}</TableCell>
+                        <TableCell>
+                          {(log.latest_data as MoneyJSONData)?.name}
+                        </TableCell>
+                        <TableCell className="-text-nowrap">
+                          {new Date(log.created_at).toLocaleString()}
+                        </TableCell>
+                        <TableCell className="truncate">
+                          {log.action === "edit_money" && log.last_data ? (
+                            <Table>
+                              <TableHeader>
+                                <TableRow>
+                                  <TableHead className="pl-0">From</TableHead>
+                                  <TableHead>To</TableHead>
+                                </TableRow>
+                              </TableHeader>
+                              <TableBody>
+                                {(log.last_data as MoneyJSONData).amount !==
+                                (log.latest_data as MoneyJSONData).amount ? (
+                                  <TableRow>
+                                    <TableCell className="pl-0">
+                                      <FaPesoSign className="text-xs min-w-fit" />
+                                      {usePhpPeso(
+                                        (log.last_data as MoneyJSONData).amount
+                                      )}{" "}
+                                    </TableCell>
+
+                                    <TableCell>
+                                      <FaPesoSign className="text-xs min-w-fit" />{" "}
+                                      {usePhpPeso(
+                                        (log.latest_data as MoneyJSONData)
+                                          .amount
+                                      )}
+                                    </TableCell>
+                                  </TableRow>
+                                ) : null}
+                                {(log.last_data as MoneyJSONData).name !==
+                                (log.latest_data as MoneyJSONData).name ? (
+                                  <TableRow>
+                                    <TableCell className="pl-0">
+                                      {(log.last_data as MoneyJSONData).name}{" "}
+                                    </TableCell>
+
+                                    <TableCell className="">
+                                      {(log.latest_data as MoneyJSONData).name}
+                                    </TableCell>
+                                  </TableRow>
+                                ) : null}
+                              </TableBody>
+                            </Table>
+                          ) : null}
+                        </TableCell>
+                        <TableCell>
+                          <div className="flex items-center">
+                            <FaPesoSign className="text-xs min-w-fit" />
+                            {usePhpPeso(log.current_total_money)}
                           </div>
-                        ) : null}
-                      </TableCell>
-                      <TableCell>
-                        <div className="flex items-center">
-                          <FaPesoSign className="text-xs min-w-fit" />
-                          {log.current_total_money}
-                        </div>
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-              <TableCaption>
-                <div className="grid grid-cols-2 w-fit">
-                  <>
-                    {action("add_money")}
-                    <p className="text-left">Add money</p>
-                  </>
-                  <>
-                    {action("edit_money")}
-                    <p className="text-left">Edit money</p>
-                  </>
-                  <>
-                    {action("del_money")}
-                    <p className="text-left">Delete money</p>
-                  </>
-                </div>
-              </TableCaption>
-            </Table>
-            <ScrollBar orientation="horizontal" />
-          </ScrollArea>
-        </CardContent>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+              <ScrollBar orientation="horizontal" />
+            </ScrollArea>
+          </CardContent>
+        </ScrollArea>
       </Card>
       <br />
       <br />
