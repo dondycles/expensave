c718206b6038cc258707ab2da5198d22551057fa
John Rod Dondoyano <81421409+dondycles@users.noreply.github.com>
2024-03-09 01:54:01 +0800

    Update get-total-money.ts

--- app/actions/get-total-money.ts
+++ app/actions/get-total-money.ts
@@ -7,17 +7,10 @@ export const getTotalMoney = async () => {
 
   const { data: session } = await supabase.auth.getSession();
   if (!session.session) return redirect("/log-in");
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  const day = date.getDate().toString().padStart(2, "0");
-
-  const dateToday = `${year}/${month}/${day}`;
 
   const { error, data } = await supabase
     .from("daily_total")
     .select("total, created_at")
-    .eq("date", dateToday)
     .single();
 
   if (error) return { error };
