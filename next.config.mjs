f75c0651274bbd2c54ac614f60894a71b662701e
dondycles <johnrod.dondoyano@gmail.com>
2024-03-12 02:18:54 +0800

    new file:   app/sw.ts 	modified:   next.config.mjs 	modified:   package.json 	modified:   pnpm-lock.yaml 	new file:   public/sw.js 	modified:   tsconfig.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const nextConfig = {};
+import withSerwistInit from "@serwist/next";
 
-export default nextConfig;
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+});
+export default withSerwist(nextConfig);
