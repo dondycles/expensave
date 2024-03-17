4eeb397cfda2aa69727ef3b00288b8fe77e3d6a7
dondycles <johnrod.dondoyano@gmail.com>
2024-03-18 00:50:20 +0800

    Remove ESLint configuration and add "million" package

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,18 @@
-/** @type {import('next').NextConfig} */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const nextConfig = {};
+import million from "million/compiler";
 import withSerwistInit from "@serwist/next";
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
 const withSerwist = withSerwistInit({
-  // Note: This is only an example. If you use Pages Router,
-  // use something else that works, such as "service-worker/index.ts".
   swSrc: "app/sw.ts",
   swDest: "public/sw.js",
   cacheOnFrontEndNav: true,
 });
-export default withSerwist(nextConfig);
-// export default nextConfig;
+const millionConfig = {
+  auto: {
+    rsc: true,
+  }, // if you're using RSC: auto: { rsc: true },
+};
+export default million.next(withSerwist(nextConfig), millionConfig);
